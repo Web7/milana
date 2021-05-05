f036231c39b7f4df88571897b74a0edddc0ae5dc
Oleksandr Nikitin <a.nikitin@i.ua>
2021-05-05 20:08:02 +0300

    Add sticky

--- src/index.js
+++ src/index.js
@@ -8,9 +8,11 @@ import bootstrap, {Dropdown} from 'bootstrap';
 import slick from 'slick-carousel';
 import L from 'leaflet';
 import PerfectScrollbar from 'perfect-scrollbar';
+import Sticky from 'sticky-js'
 
 window.PerfectScrollbar = PerfectScrollbar;
 window.bootstrap = bootstrap;
 window.Dropdown = Dropdown;
+window.Sticky = Sticky;
 
 import './js/common.js';
